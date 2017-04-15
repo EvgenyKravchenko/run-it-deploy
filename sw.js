09c4ddf018ec18bb73785d0c6ccb8bb6e56745a1
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-04-15 21:49:43 +0300

    Add offline support

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('runit').then(cache => cache.addAll([
+      '/',
+      '/now',
+      'Anton-Korchynskyi.jpg',
+      'Antonyuk.jpg',
+      'bg.jpg',
+      'Chesnokov.jpg',
+      'coffee.jpeg',
+      'coffee.png',
+      'Dmitry-Voloshko.jpg',
+      'logo.png',
+      'logo-144x144.png',
+      'Luca-Merolla.jpg',
+      'lunch.png',
+      'Mariia-Kot.jpg',
+      'Max-Itskovich.jpg',
+      'Max-Sabarnya.jpg',
+      'Oleksandra-Alkhimovych.jpg',
+      'Olga-Ivanova.jpg',
+      'Orkhan-Gasimov.jpg',
+      'Vera-Shatskaya.jpg',
+      'assets/run-it.css',
+      'assets/run-it.js',
+      'assets/vendor.css',
+      'assets/vendor.js'
+    ]))
+  );
+});
